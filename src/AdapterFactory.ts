87241f911554479f6b141cfafc039e70048265f7
Alexandr Alexandrov <alxcube@gmail.com>
2023-12-06 08:45:28 +0200

    Added doc comments to AdapterFactory.ts

--- src/AdapterFactory.ts
+++ src/AdapterFactory.ts
@@ -2,11 +2,20 @@ import Jimp from "jimp";
 import type { ImageAdapter, ImageAdapterFactory } from "../../lens";
 import { Adapter } from "./Adapter";
 
+/**
+ * Jimp ImageAdapter factory class.
+ */
 export class AdapterFactory implements ImageAdapterFactory<Jimp> {
+  /**
+   * @inheritDoc
+   */
   match(resource: unknown): boolean {
     return resource instanceof Jimp;
   }
 
+  /**
+   * @inheritDoc
+   */
   create(resource: Jimp): ImageAdapter<Jimp> {
     return new Adapter(resource);
   }
